3ff6e99437be20a9160b414c4ddadc61018b4b46
Yara Van Someren <yara.vansomeren@mrc-cbu.cam.ac.uk>
2014-03-19 16:04:20 +0000

    support no threshold

--- thresholdrois.m
+++ thresholdrois.m
@@ -4,16 +4,28 @@
 % processing after drawing the ROI in FSLView.
 %
 % outfiles = thresholdrois(maskpath,rois,outdir)
-function outfiles = thresholdrois(maskpath,rois,outdir)
+function outfiles = thresholdrois(maskpath,rois,outdir,threshold)
 
 maskV = spm_vol(maskpath);
 mask = spm_read_vols(maskV);
 mask(isnan(mask)) = 0;
 
-if ~exist('outdir','var') || isempty(outdir)
+if ieNotDefined('outdir')
     outdir = 'thresholded';
 end
 
+if ieNotDefined('threshold')
+    threshold = 0;
+end
+
+if threshold==0
+    op = @(x,y)x~=y;
+elseif threshold>0
+    op = @gt;
+else
+    op = @lt;
+end
+    
 outfiles = {};
 for roi = rois(:)'
     roistr = roi{1};
@@ -29,12 +41,14 @@ for roi = rois(:)'
         roistr = newroipath;
         [path,fn,ext] = fileparts(roistr);
     end
+    % roi binary mask
     roiV = spm_vol(roistr);
     roi = spm_read_vols(roiV);
     roi(isnan(roi)) = 0;
     maskedroi = mask;
+    roi = roi~=0 & op(maskedroi,threshold);
     % to preserve float in mask, we set values outside the ROI to 0
-    maskedroi(roi==0) = 0;
+    maskedroi(~roi) = 0;
     roiV.fname = fullfile(roidir,outdir,[fn ext]);
     mkdirifneeded(fullfile(roidir,outdir));
     roiV.dt = maskV.dt;
