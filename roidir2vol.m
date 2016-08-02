6bb74b9f4865a884b87b123c83be394aa7e914da
Johan Carlin <johan.carlin@gmail.com>
2016-08-02 15:44:23 +0100

    use stripextension in place of deprecated stripext

--- roidir2vol.m
+++ roidir2vol.m
@@ -1,8 +1,12 @@
+% construct a pilab roivol with one region for each NII file encountered in
+% roidir.
+%
+% roivol = roidir2vol(roidir)
 function roivol = roidir2vol(roidir)
 
 rois = dir(fullfile(roidir,'*.nii'));
 nroi = length(rois);
-names = stripext({rois.name}');
+names = stripextension({rois.name}');
 cwd = pwd;
 cd(roidir);
 roivol = SPMVolume({rois.name},[],'metasamples',struct('names',{names}));
